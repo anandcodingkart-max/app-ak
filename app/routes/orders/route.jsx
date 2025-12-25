d0cf5ab37a2bda96c923f0edc479b088428092ee
anandcodingkart-max <anand.codingkart@gmail.com>
2025-12-25 13:58:21 +0530

    order update webhook

--- app/routes/orders/route.jsx
+++ app/routes/orders/route.jsx
@@ -1,21 +1,6 @@
-import { shopifyApi } from "@shopify/shopify-api";
 import { authenticate } from "../../shopify.server";
 import { cors } from "remix-utils/cors";
 import { orderQueue } from "../../queues/order.server";
-
-const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY;
-const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET;
-const SHOPIFY_APP_URL = process.env.SHOPIFY_APP_URL;
-const API_VERSION = "2025-01";
-
-export const shopify = shopifyApi({
-  apiKey: SHOPIFY_API_KEY,
-  apiSecretKey: SHOPIFY_API_SECRET,
-  scopes: ["write_orders"],
-  hostName: SHOPIFY_APP_URL.replace(/^https?:\/\//, ""),
-  apiVersion: API_VERSION,
-  isEmbeddedApp: false,
-});
 
 export async function loader({ request }) {
   if (request.method === "OPTIONS") {
